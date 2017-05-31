3ea050a763caf66211d53125b761645981a6b60c
JuKu <kuenzel.justin@t-online.de>
2017-05-31 23:10:16 +0200

    added main class for management server.

---
+++ ManagementServer/src/main/java/com/jukusoft/rpgcreator/server/mancenter/ServerMain.java
@@ -0,0 +1,12 @@
+package com.jukusoft.rpgcreator.server.mancenter;
+
+/**
+ * Created by Justin on 31.05.2017.
+ */
+public class ServerMain {
+
+    public static void main (String[] args) {
+        //
+    }
+
+}
