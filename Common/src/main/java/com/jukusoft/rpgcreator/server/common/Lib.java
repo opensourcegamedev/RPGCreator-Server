5b522d146e88fb6fdfc0ee4eec6ffaf2517718f2
JuKu <kuenzel.justin@t-online.de>
2017-06-26 00:13:20 +0200

    added some basic classes.

--- Common/src/main/java/com/jukusoft/rpgcreator/server/common/Lib.java
+++
@@ -1,7 +0,0 @@
-package com.jukusoft.rpgcreator.server.common;
-
-/**
- * Created by Justin on 31.05.2017.
- */
-public class Lib {
-}
---
+++ Common/src/main/java/com/jukusoft/rpgcreator/server/common/json/JSONLoadable.java
@@ -0,0 +1,12 @@
+package com.jukusoft.rpgcreator.server.common.json;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Justin on 10.02.2017.
+ */
+public interface JSONLoadable {
+
+    public void loadFromJSON(JSONObject json);
+
+}
