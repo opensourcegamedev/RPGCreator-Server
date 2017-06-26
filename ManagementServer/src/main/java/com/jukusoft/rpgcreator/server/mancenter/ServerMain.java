98ccac352eff0e8c5217fdeaba2b69458912602d
JuKu <kuenzel.justin@t-online.de>
2017-06-26 15:51:48 +0200

    added support to connect to mysql server.

--- ManagementServer/src/main/java/com/jukusoft/rpgcreator/server/mancenter/ServerMain.java
+++ ManagementServer/src/main/java/com/jukusoft/rpgcreator/server/mancenter/ServerMain.java
@@ -1,10 +1,13 @@
 package com.jukusoft.rpgcreator.server.mancenter;
 
 import com.jukusoft.rpgcreator.server.common.database.config.MySQLConfig;
+import com.jukusoft.rpgcreator.server.common.database.mysql.DefaultMySQLServer;
+import com.jukusoft.rpgcreator.server.common.database.mysql.MySQLServer;
 import com.jukusoft.rpgcreator.server.mancenter.network.impl.ManCenterServer;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
 
 /**
  * Created by Justin on 31.05.2017.
@@ -36,7 +39,17 @@ public class ServerMain {
             System.exit(1);
         }
 
-        //
+        System.out.println("try to connect to mysql server " + mySQLConfig.getDBUrl() + ".");
+
+        //create database connection
+        MySQLServer mySQLServer = new DefaultMySQLServer();
+
+        try {
+            mySQLServer.connect(mySQLConfig);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
 
         System.out.println("try to start management server on port " + port + "now.");
 
